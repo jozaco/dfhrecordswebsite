16c187858a11dbf35b625dbeebf3e7221953d6ce
Joel Zarrias Conchillo <Joel@MacBook-Air-de-Joel.local>
2020-05-23 12:28:31 +0200

    Initial static website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav></nav>
       </header>
+      <div className="main">
+        <img src="assets/images/logo.png" alt="DFH Records" />
+        <p>
+          <a href="mailto:woof@dfhrecords.com" title="Write us an email">woof@dfhrecords.com</a>
+        </p>
+      </div>
     </div>
   );
 }
